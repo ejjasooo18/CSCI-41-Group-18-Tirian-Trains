219fa076759e4fb8d18a7c0f5b5d72512c6aa86b
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-11-27 23:01:21 +0800

    Add empty urls.py to main

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+# from .views import (ThreadListView,
+#                     ThreadDetailView,
+#                     ThreadCreateView,
+#                     ThreadUpdateView)
+
+urlpatterns = [
+]
+
+app_name = 'main'
