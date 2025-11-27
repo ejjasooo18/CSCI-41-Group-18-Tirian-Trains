be0b405d53d40a27d661af172963a83681e4470b
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-11-27 20:58:12 +0800

    Add empty classes for models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,43 @@
+from datetime import timedelta
+
 from django.db import models
 
+ROUTE_TYPE = {
+    'INT': 'Inter-town',
+    'LOC': 'Local'
+}
+
+
+class Route(models.Model):
+    price = models.PositiveIntegerField(default=0)
+    travel_time = models.DurationField(default=timedelta(minutes=5))
+    route_type = models.CharField(default='INT', choices=ROUTE_TYPE)
+
+
+class Station(models.Model):
+    pass
+
+
+class Trip(models.Model):
+    pass
+
+
+class Customer(models.Model):
+    pass
+
+
+class Ticket(models.Model):
+    pass
+
+
+class Train(models.Model):
+    pass
+
+
+class MaintenanceEvent(models.Model):
+    pass
+
+
+class CrewMember(models.Model):
+    pass
 
