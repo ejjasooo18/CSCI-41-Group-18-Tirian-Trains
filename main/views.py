b94d642e1236b0a4b238a4455a01b95993ed7cd7
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-11-29 03:45:28 +0800

    added trip_list function
    fetches the trips

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Trip, Ticket, Customer
 
-# Create your views here.
+def trip_list(request):
+    # fetches all the trips ordered by trip date and departure time.
+    trips = Trip.objects.all().order_by('trip_date', 'departure_time')
+    return render(request, 'main/trip_list.html', {'trips': trips})
+
